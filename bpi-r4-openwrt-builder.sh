b3479de99955f2aa34ad5037d00864da5a3042e8
jfrag <g.garenne@gmail.com>
2026-03-30 20:25:00 +0000

    create etx4 LXC partition

--- bpi-r4-openwrt-builder.sh
+++ bpi-r4-openwrt-builder.sh
@@ -40,6 +40,7 @@ chmod -R 755 feeds/luci/applications/luci-app-sms-tool-js/root
 chmod -R 755 feeds/packages/utils/modemdata/files/usr/share
 
 \cp -r ../configs/my_final_defconfig .config
+[ -d ../my_files/openwrt-files ] && \cp -r ../files
 make defconfig
 
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-mt798x_rfb-wifi7_nic build
